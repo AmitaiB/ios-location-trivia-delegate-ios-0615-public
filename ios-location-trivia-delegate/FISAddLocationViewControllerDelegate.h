6103f4aa2c4444a3fd58fef13220b4a9f088173e
AmitaiB <blickstein@gmail.com>
2015-06-28 20:10:45 -0400

    adds FISAddLocationViewControllerDelegate (empty)

---
+++ ios-location-trivia-delegate/FISAddLocationViewControllerDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FISAddLocationViewControllerDelegate.h
+//  ios-location-trivia-delegate
+//
+//  Created by Amitai Blickstein on 6/28/15.
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol FISAddLocationViewControllerDelegate <NSObject>
+
+
+
+@end
